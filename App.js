ad9b1fc09f28c16b2845b7a040a500d54a5d2a59
roxanaldwg <roxana_ludwig@yahoo.de>
2022-04-07 18:40:05 +0200

    reset existing app.js and implemented 2 Screens as components

--- App.js
+++ App.js
@@ -1,51 +1,32 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet } from "react-native";
+import React from "react";
+
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// create navigator
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
-  }
-
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(text) => this.setState({ text })}
-          value={this.state.text}
-          placeholder='Type here ...'
-        />
-        <Text>You wrote: {this.state.text}</Text>
-        <Button
-          onPress={() => {
-            this.alertMyText({ text: this.state.text });
-          }}
-          title="Press Me"
-        />
-        <ScrollView>
-          <Text style={{ fontSize: 110 }}>This text is so big! And so long! You have to scroll!</Text>
-        </ScrollView>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Screen1"
+        >
+          <Stack.Screen
+            name="Screen1"
+            component={Screen1}
+          />
+          <Stack.Screen
+            name="Screen2"
+            component={Screen2}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column'
-  },
-  box1: {
-    flex: 10,
-    backgroundColor: 'blue'
-  },
-  box2: {
-    flex: 80,
-    backgroundColor: 'red'
-  },
-  box3: {
-    flex: 50,
-    backgroundColor: 'green'
-  }
-});
