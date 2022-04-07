20a30e5f845baf591b7f2e0a796ec5173f1398d3
roxanaldwg <roxana_ludwig@yahoo.de>
2022-04-07 17:21:50 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TextInput, Button, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+          placeholder='Type here ...'
+        />
+        <Text>You wrote: {this.state.text}</Text>
+        <Button
+          onPress={() => {
+            this.alertMyText({ text: this.state.text });
+          }}
+          title="Press Me"
+        />
+        <ScrollView>
+          <Text style={{ fontSize: 110 }}>This text is so big! And so long! You have to scroll!</Text>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column'
   },
+  box1: {
+    flex: 10,
+    backgroundColor: 'blue'
+  },
+  box2: {
+    flex: 80,
+    backgroundColor: 'red'
+  },
+  box3: {
+    flex: 50,
+    backgroundColor: 'green'
+  }
 });
